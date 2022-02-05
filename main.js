3c9c5797e823aaaca7ad7a1786bb0365d29ebcee
sohilp2010 <77651250+sohilp2010@users.noreply.github.com>
2022-02-04 19:36:49 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
+leftWristX = 0;
+rightWristX = 0;
+difference = 0;
 function setup()
     {
         video = createCapture(VIDEO);
         video.size(550, 500);
 
-        canvas= createCanvas(550, 550);
+        canvas= createCanvas(550, 450);
         canvas.position(560, 150);
 
         poseNet = ml5.poseNet(video, modelLoaded);
@@ -18,5 +21,16 @@ function setup()
         if(results.length > 0)
         {
             console.log(results);
+            leftWristX = results[0].pose.leftWrist.x;
+            rightWristX = results[0].pose.rightWrist.x;
+            difference = floor(leftWristX - rightWristX);
         }
+    }
+    function draw()
+    {
+        //red
+        background('#FF0000');
+        textSize(difference);
+        fill('#0000FF');
+        text('Sohil', 50, 200);
     }
